a8886637875192500d78a274f23f9197fa3de31a
Heng Li <lh3@me.com>
2022-08-24 20:54:03 -0400

    refactor

--- nasw-f.c
+++ nasw-f.c
@@ -13,6 +13,59 @@
 
 #define ns_sse_i(x, i) ((NS_SSE_INT*)(&(x)))[(i)]
 
+static uint8_t *ns_prep(void *km, const char *ns, int32_t nl, const char *as, int32_t al, const ns_opt_t *opt, uint8_t **aas_, int8_t **donor_, int8_t **acceptor_)
+{
+	int32_t i, j, l;
+	uint8_t codon, *nas, *aas;
+	int8_t *donor, *acceptor;
+	nas = Kmalloc(km, uint8_t, nl + al + (nl + 1) * 2);
+	*aas_ = aas = nas + nl;
+	*donor_ = donor = (int8_t*)aas + al, *acceptor_ = acceptor = donor + nl + 1;
+	for (j = 0; j < al; ++j) // generate aas[]
+		aas[j] = opt->aa20[(uint8_t)as[j]];
+	for (i = 0; i < nl; ++i) // nt4 encoding of ns[] for computing donor[] and acceptor[]
+		nas[i] = opt->nt4[(uint8_t)ns[i]];
+	for (i = 0; i < nl + 1; ++i)
+		donor[i] = acceptor[i] = opt->nc;
+	for (i = 0; i < nl - 3; ++i) { // generate donor[]
+		int32_t t = 0;
+		if (nas[i+1] == 2 && nas[i+2] == 3) t = 1;
+		if (t && i + 3 < nl && (nas[i+3] == 0 || nas[i+3] == 2)) t = 2;
+		donor[i] = t == 2? 0 : t == 1? opt->nc/2 : opt->nc;
+	}
+	for (i = 1; i < nl; ++i) { // generate acceptor[]
+		int32_t t = 0;
+		if (nas[i-1] == 0 && nas[i] == 2) t = 1;
+		if (t && i > 0 && (nas[i-2] == 1 || nas[i-2] == 3)) t = 2;
+		acceptor[i] = t == 2? 0 : t == 1? opt->nc/2 : opt->nc;
+	}
+	memset(nas, opt->aa20['X'], nl);
+	for (i = l = 0, codon = 0; i < nl; ++i) { // generate the real nas[]
+		uint8_t c = opt->nt4[(uint8_t)ns[i]];
+		if (c < 4) {
+			codon = (codon << 2 | c) & 0x3f;
+			if (++l >= 3)
+				nas[i] = opt->codon[codon];
+		} else codon = 0, l = 0;
+	}
+	return nas;
+}
+
+#define ns_gen_prof(INT_TYPE, _km, aas, al, opt, neg_inf, _mem_ap, _ap) do { \
+	INT_TYPE *t; \
+	int32_t a, p = 16 / sizeof(INT_TYPE), slen = (al + p - 1) / p; \
+	*(_mem_ap) = Kmalloc(_km, uint8_t, (16 * slen * opt->asize + 31) / 16 * 16); \
+	*(_ap) = (__m128i*)(((size_t)*(_mem_ap) + 15) / 16 * 16); /* 16-byte aligned */ \
+	t = (INT_TYPE*)*(_ap); \
+	for (a = 0; a < opt->asize; ++a) { \
+		int32_t i, k, nlen = slen * p; \
+		const int8_t *ma = opt->sc + a * opt->asize;\
+		for (i = 0; i < slen; ++i) \
+			for (k = i; k < nlen; k += slen) \
+				*t++ = (k >= al? neg_inf : ma[aas[k]]); \
+	} \
+} while (0)
+
 void ns_splice_i16(void *km, const char *ns, int32_t nl, const char *as, int32_t al, const ns_opt_t *opt, ns_rst_t *r)
 {
 	int32_t i, j, slen = (al + NS_SSE_P - 1) / NS_SSE_P; // segment length
@@ -21,56 +74,8 @@ void ns_splice_i16(void *km, const char *ns, int32_t nl, const char *as, int32_t
 	__m128i *ap;
 
 	r->n_cigar = 0;
-
-	{ // generate nas[], aas[], donor[] and acceptor[]
-		int32_t l;
-		uint8_t codon;
-		nas = Kmalloc(km, uint8_t, nl + al + (nl + 1) * 2);
-		aas = nas + nl;
-		donor = (int8_t*)aas + al, acceptor = donor + nl + 1;
-		for (j = 0; j < al; ++j) // generate aas[]
-			aas[j] = opt->aa20[(uint8_t)as[j]];
-		for (i = 0; i < nl; ++i) // nt4 encoding of ns[] for computing donor[] and acceptor[]
-			nas[i] = opt->nt4[(uint8_t)ns[i]];
-		for (i = 0; i < nl + 1; ++i)
-			donor[i] = acceptor[i] = opt->nc;
-		for (i = 0; i < nl - 3; ++i) { // generate donor[]
-			int32_t t = 0;
-			if (nas[i+1] == 2 && nas[i+2] == 3) t = 1;
-			if (t && i + 3 < nl && (nas[i+3] == 0 || nas[i+3] == 2)) t = 2;
-			donor[i] = t == 2? 0 : t == 1? opt->nc/2 : opt->nc;
-		}
-		for (i = 1; i < nl; ++i) { // generate acceptor[]
-			int32_t t = 0;
-			if (nas[i-1] == 0 && nas[i] == 2) t = 1;
-			if (t && i > 0 && (nas[i-2] == 1 || nas[i-2] == 3)) t = 2;
-			acceptor[i] = t == 2? 0 : t == 1? opt->nc/2 : opt->nc;
-		}
-		memset(nas, opt->aa20['X'], nl);
-		for (i = l = 0, codon = 0; i < nl; ++i) { // generate the real nas[]
-			uint8_t c = opt->nt4[(uint8_t)ns[i]];
-			if (c < 4) {
-				codon = (codon << 2 | c) & 0x3f;
-				if (++l >= 3)
-					nas[i] = opt->codon[codon];
-			} else codon = 0, l = 0;
-		}
-	}
-
-	{ // generate protein profile
-		NS_SSE_INT *t;
-		int32_t a;
-		mem_ap = Kmalloc(km, uint8_t, (16 * slen * opt->asize + 31) / 16 * 16);
-		ap = (__m128i*)(((size_t)mem_ap + 15) / 16 * 16); // 16-byte aligned
-		t = (NS_SSE_INT*)ap;
-		for (a = 0; a < opt->asize; ++a) {
-			int32_t i, k, nlen = slen * NS_SSE_P;
-			const int8_t *ma = opt->sc + a * opt->asize;
-			for (i = 0; i < slen; ++i)
-				for (k = i; k < nlen; k += slen) // p iterations
-					*t++ = (k >= al? NS_SSE_NEG_INF : ma[aas[k]]);
-		}
-	}
+	nas = ns_prep(km, ns, nl, as, al, opt, &aas, &donor, &acceptor);
+	ns_gen_prof(int16_t, km, aas, al, opt, NS_SSE_NEG_INF, &mem_ap, &ap);
 
 	/*
 	 * I(i,j) = max{ H(i,j-1) - q, I(i,j-1) } - e
