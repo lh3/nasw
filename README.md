84e98b7e167e528679df2739d135eb41f87cf1f1
Heng Li <lh3@me.com>
2022-08-27 15:40:12 -0400

    added README; polished CLI

---
+++ README.md
@@ -0,0 +1,25 @@
+## Getting Started
+```sh
+git clone https://github.com/lh3/nasw
+cd nasw && make
+./nasw test/test-n.fa test/test-p.fa
+./nasw test/DPP3-hs.gen.fa.gz test/DPP3-mm.pep.fa.gz
+```
+
+## Introduction
+
+nasw provides a **proof-of-concept** implementation of dynamic programming (DP)
+for protein-to-genome alignment with affine-gap penalty, splicing and
+frameshifts. The DP involves 6 states and 20 transitions, similar to the
+[GeneWise][genewise] model. Different from GeneWise, nasw explicitly implements
+the DP recursion with SSE4.1 intrinsics and is tens of times faster.
+
+## Limitations
+
+1. The initial condition disallows gap opens at the beginning of the sequences.
+2. Global alignment only
+3. Simple splice site model
+4. Requiring SSE4.1 for blendv
+5. The 32-bit mode uses excessive memory for traceback
+
+[genewise]: https://pubmed.ncbi.nlm.nih.gov/15123596/
--- main.c
+++ main.c
@@ -13,16 +13,22 @@ int main(int argc, char *argv[])
 	kseq_t *ksn, *ksa;
 	ketopt_t o = KETOPT_INIT;
 	ns_opt_t opt;
-	int32_t c, use_sse = 0;
+	int32_t c, no_sse = 0, use_32 = 0;
 
 	ns_make_tables(0);
 	ns_opt_init(&opt);
-	while ((c = ketopt(&o, argc, argv, 1, "fc", 0)) >= 0) {
-		if (c == 'f') use_sse = 1;
-		else if (c == 'c') opt.flag |= NS_F_CIGAR;
+	opt.flag |= NS_F_CIGAR;
+	while ((c = ketopt(&o, argc, argv, 1, "lsw", 0)) >= 0) {
+		if (c == 'l') no_sse = 1;
+		else if (c == 's') opt.flag &= ~NS_F_CIGAR;
+		else if (c == 'w') use_32 = 1;
 	}
 	if (argc - o.ind < 2) {
 		fprintf(stderr, "Usage: nasw [options] <nt.fa> <aa.fa>\n");
+		fprintf(stderr, "Options:\n");
+		fprintf(stderr, "  -w       use 32-bit integers for scoring (16-bit by default)\n");
+		fprintf(stderr, "  -s       compute score only without nasw-CIGAR\n");
+		fprintf(stderr, "  -l       non-SSE mode\n");
 		return 1;
 	}
 
@@ -37,8 +43,9 @@ int main(int argc, char *argv[])
 		ns_rst_t r;
 		int32_t i;
 		ns_rst_init(&r);
-		if (use_sse) ns_global_gs16(0, ksn->seq.s, ksn->seq.l, ksa->seq.s, ksa->seq.l, &opt, &r);
-		else ns_splice_s1(0, ksn->seq.s, ksn->seq.l, ksa->seq.s, ksa->seq.l, &opt, &r);
+		if (no_sse) ns_splice_s1(0, ksn->seq.s, ksn->seq.l, ksa->seq.s, ksa->seq.l, &opt, &r);
+		else if (use_32) ns_global_gs32(0, ksn->seq.s, ksn->seq.l, ksa->seq.s, ksa->seq.l, &opt, &r);
+		else ns_global_gs16(0, ksn->seq.s, ksn->seq.l, ksa->seq.s, ksa->seq.l, &opt, &r);
 		printf("%s\t%ld\t%s\t%ld\t%d\t", ksn->name.s, ksn->seq.l, ksa->name.s, ksa->seq.l, r.score);
 		for (i = 0; i < r.n_cigar; ++i)
 			printf("%d%c", r.cigar[i]>>4, NS_CIGAR_STR[r.cigar[i]&0xf]);
